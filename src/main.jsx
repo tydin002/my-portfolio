b165f75c9036315086d9d9ccb36b3d1083bdb8c4
Ha'ani Tydingco <tydin002@csusm.edu>
2026-04-16 22:06:35 -0700

    fixing url

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '');
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename || undefined}>
+    <BrowserRouter basename="/my-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
